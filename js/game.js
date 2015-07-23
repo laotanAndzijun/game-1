00c69a6f341cdbf7536849a9cfabd094cc998483
Longyu Jiang <langren1993@hotmail.com>
2015-07-23 12:11:03 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -3,6 +3,26 @@
  * 根据 http://www.cnblogs.com/Wayou/p/how-to-make-a-simple-html5-canvas-game.html 改编
  * 1.加载角色
  */
+ //招财猫
+ var zcm = {
+    speed: 200,
+    x: 0,
+    y: 0
+
+};
+function yb(){
+     var speed=5,
+        x=0;
+        y=0;
+}
+yb.prototype.init = function(){
+	this.speed = 3;
+	this.y = -canvas.height * 2 * Math.random();
+	this.x = (canvas.width+50) * Math.random()-33;
+}
+var ybs = [];
+ //元宝
+ 
 //获取canvas对象
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
@@ -48,6 +68,16 @@ var render =function(){
 	else{
 		console.log('加载失败');
 	}
+}
+//游戏初始化
+var reset = function () {
+    zcm.x = canvas.width / 2 - 89;//猫居中
+    zcm.y = canvas.height - 111;
+
+    for (var i = 0,ybsl=5;i < ybsl; i++) {
+        ybs[i] = new c_yb();
+        ybs[i].init();
+    }
 }
 //主函数
 var main=function(){
