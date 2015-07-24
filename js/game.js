27e84abee94eac07a4f255db3126589655d81768
Longyu Jiang <langren1993@hotmail.com>
2015-07-24 09:15:08 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -89,6 +89,7 @@ var reset = function () {
 addEventListener('touchstart',touchSatrtFunc,false);
 function touchSatrtFunc(e) {  
     e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等  
+    var touch = e.touches[0]; //获取第一个触点  
     var x = Number(touch.pageX); //页面触点X坐标  
     var y = Number(touch.pageY); //页面触点Y坐标  
     //记录触点初始位置  
