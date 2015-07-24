1b062f2ebac82de306a5227c573906974dba9fcb
Longyu Jiang <langren1993@hotmail.com>
2015-07-24 12:29:11 +0800

    Update game.js

--- js/game.js
+++ js/game.js
@@ -87,18 +87,14 @@ var reset = function () {
 }
 //拖动角色
 addEventListener('touchstart',touchSatrtFunc,false);
-function touchSatrtFunc(e,modifier) {  
+function touchSatrtFunc(e) {  
     e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等  
-    var touch = e.touches[0]; //获取第一个触点  
+    var touch = e.touches; 
+    console.log(touch)//获取第一个触点  
     var x = Number(touch.pageX); //页面触点X坐标  
     var y = Number(touch.pageY); //页面触点Y坐标  
     //记录触点初始位置  
-    if (zcm.x>x) { // 猫的左侧
-        zcm.x -= zcm.speed * modifier;
-    }
-    if (zcm.x<x) { // 在猫的右侧
-            zcm.x += zcm.speed * modifier;
-    }
+    
 } 
 
 
@@ -108,17 +104,12 @@ function touchSatrtFunc(e,modifier) {
 
 //主函数
 var main = function () {
-    var now = Date.now();
-    var delta = now - then;
-    time = (time * 1000 - delta) / 1000;
-    touchSatrtFunc(delta / 1000);
-    render();
-    then = now;
+    
     // 立即调用主函数
     requestAnimationFrame(main);
 };
 
-var then = Date.now();
+
 reset();
 
 main();
